390226017372f18cbaf70118d2d498319369f6b4
Taylor <taylor@crucialartists.com>
2023-03-20 12:29:45 -0500

    Create sessions controller and build out users contoller methods

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,32 @@
+class SessionsController < ApplicationController
+    
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+    # before_action :authorize
+    # skip_before_action :authorize, only: [:create, :destroy]
+    
+        def create
+            user = User.find_by(username: params[:username])
+            if user&.authenticate(params[:password])
+                session[:user_id] = user.id
+                render json: user, status: :created
+            else
+                render json: { errors: ["Invalid email or password"] }, status: :unauthorized
+            end
+        end
+    
+        def destroy
+            session.delete :user_id
+            head :no_content
+        end
+    
+        private
+    
+        # def authorize
+        #     return render json: { errors: ["Not authorized"] }, status: :unauthorized unless session.include? :user_id
+        # end
+        
+        def render_unprocessable_entity_response(invalid)
+            render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+        end
+
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,41 @@
 class UsersController < ApplicationController
 
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+    before_action :authorize
+    skip_before_action :authorize, only: [:index, :create]
+
     def index
         users = User.all
         render json: users
+    end
+
+    def show
+        user = User.find(session[:user_id])
+        render json: user
+    end
+
+    def create
+        user = User.create!(user_params)
+        if user.valid?
+            session[:user_id] = user.id
+            render json: user
+        else
+            render :render_unprocessable_entity_response
+        end
+    end
+
+    private
+
+    def user_params
+        params.permit(:username, :password, :bio)
+    end
+
+    def authorize
+        return render json: { errors: ["Whoops! Please log in to continue."] }, status: :unauthorized unless session.include? :user_id
+    end
+
+    def render_unprocessable_entity_response(invalid)
+        render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
     end
 
 end
