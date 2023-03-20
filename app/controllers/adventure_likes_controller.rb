3c0f9caed63bbb1f29f147a7c9ba7067b4f39c5c
Taylor <taylor@crucialartists.com>
2023-03-20 12:11:43 -0500

    Add index and create to adventure_likes controller

--- app/controllers/adventure_likes_controller.rb
+++ app/controllers/adventure_likes_controller.rb
@@ -1,2 +1,19 @@
 class AdventureLikesController < ApplicationController
+
+    def index
+        adventure_likes = AdventureLike.all
+        render json: adventure_likes
+    end
+
+    def create
+        adventure_like = AdventureLike.create!(user_params)
+        render json: adventure_like
+    end
+
+    private
+
+    def user_params
+        params.permit(:user_id, :adventure_id)
+    end
+
 end
