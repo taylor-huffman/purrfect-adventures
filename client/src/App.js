e5ca7d5774938ab5d8902ea83b1c93f3bd9528cd
Taylor <taylor@crucialartists.com>
2023-03-20 12:47:12 -0500

    Update route handling in App.js, add providers to index.js, add smooth scroll to app.css

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,101 @@
-import logo from './logo.svg';
 import './App.css';
+import Nav from './components/Nav';
+import React, { useContext } from 'react'
+import Account from './components/Account'
+import Adventures from './components/Adventures'
+import Login from './components/Login'
+import { Route, Routes } from 'react-router-dom'
+import { UserContext } from './context/user';
+import { Navigate } from 'react-router-dom'
+import { Bars } from 'react-loader-spinner'
+import CreateAdventure from './components/CreateAdventure';
 
 function App() {
+
+  const { user, isAuth, isLoading, authErrors, setAuthErrors } = useContext(UserContext)
+
+  const spinner = <div style={{ width: '100%', height: '100%' }}>
+    <div style={{ position: 'absolute', top: '40%', left: 'calc(50% - 30px)' }}>
+      <Bars
+        height="50"
+        width="50"
+        color="#000000"
+        ariaLabel="bars-loading"
+        wrapperStyle={{}}
+        wrapperClass=""
+        visible={true}
+      />
+    </div>
+  </div>
+
+  console.log(isAuth)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ minHeight: '100vh', display: 'flex', flexDirection: 'column' }}>
+      {/* <Nav /> */}
+      <Routes>
+        <Route path='/' element={
+          isLoading ? spinner : isAuth ? (
+            <Adventures />
+          ) : (
+            <Login authErrors={authErrors} setAuthErrors={setAuthErrors} />
+            // <Navigate to="/login" authErrors={authErrors} setAuthErrors={setAuthErrors} />
+          )
+        }/>
+        <Route path='/adventures' element={
+          isLoading ? spinner : isAuth ? (
+            <Adventures />
+          ) : (
+            <Login authErrors={authErrors} setAuthErrors={setAuthErrors} />
+            // <Navigate to="/login" authErrors={authErrors} setAuthErrors={setAuthErrors} />
+          )
+        }/>
+        <Route path='/account' element={
+          isLoading ? spinner : isAuth ? (
+            <Account />
+          ) : (
+            <Login authErrors={authErrors} setAuthErrors={setAuthErrors} />
+            // <Navigate to="/login" authErrors={authErrors} setAuthErrors={setAuthErrors} />
+          )
+        }/>
+        <Route path='/create/adventure' element={
+          isLoading ? spinner : isAuth ? (
+            <CreateAdventure />
+          ) : (
+            <Login authErrors={authErrors} setAuthErrors={setAuthErrors} />
+            // <Navigate to="/login" authErrors={authErrors} setAuthErrors={setAuthErrors} />
+          )
+        }/>
+        <Route path='/login' element={<Login/>}/>
+        {/* <Route exact path="/login">
+            <Login/>
+        </Route>
+        <Route exact path="/account">
+            {isLoading ? spinner : (
+                function() {
+                    if (isAuth) {
+                        return <Account/>
+                    } else {
+                        return <Navigate to="/login" />
+                    }
+                }
+            ) }
+        </Route>
+        <Route exact path="/adventures">
+            {isLoading ? spinner : (
+                function() {
+                    if (isAuth) {
+                        return <Adventures />
+                    } else {
+                        return <Navigate to="/login" />
+                    }
+                }
+            ) }
+        </Route>
+        <Route exact path="/">
+            <Adventures />
+        </Route> */}
+      </Routes>
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,120 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { UserProvider } from '../src/context/user';
+import { ThemeProvider, createTheme } from '@mui/material/styles'
+import { BrowserRouter } from 'react-router-dom';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { LocalizationProvider } from '@mui/x-date-pickers';
+
+const theme = createTheme({
+  palette: {
+    type: 'light',
+    primary: {
+      main: '#000000',
+      dark: '#000000',
+      light: '#f8f8f8',
+      contrastText: '#f8f8f8',
+    },
+    secondary: {
+      main: '#ebebeb',
+      contrastText: '#231f20',
+    },
+    text: {
+      primary: '#000000',
+      secondary: '#000000',
+      disabled: '#949494',
+      hint: '#231f20',
+    },
+  },
+  typography: {
+    h1: {
+      fontWeight: 500,
+    },
+    h2: {
+      fontFamily: 'Poppins',
+      fontWeight: 500,
+    },
+    h3: {
+      fontFamily: 'Poppins',
+      fontWeight: 500,
+    },
+    h4: {
+      fontFamily: 'Poppins',
+      fontWeight: 500,
+    },
+    h5: {
+      fontFamily: 'Poppins',
+      fontWeight: 500,
+    },
+    h6: {
+      fontFamily: 'Poppins',
+      fontWeight: 500,
+    },
+    subtitle1: {
+      fontFamily: 'Poppins',
+    },
+    subtitle2: {
+      fontFamily: 'Poppins',
+    },
+    body1: {
+      fontFamily: 'Poppins',
+    },
+    body2: {
+      fontFamily: 'Poppins',
+    },
+    button: {
+      fontFamily: 'Poppins',
+      fontWeight: 500,
+    },
+    caption: {
+      fontFamily: 'Poppins',
+    },
+    overline: {
+      fontFamily: 'Poppins',
+    },
+    fontFamily: 'Poppins',
+    fontWeightBold: 500,
+    fontSize: 16,
+  },
+  overrides: {
+    MuiAppBar: {
+      root: {
+        backgroundColor: '#fdfdfd!important',
+        color: '#231f20!important',
+        boxShadow: 'unset',
+      },
+    },
+    MuiInputBase: {
+      root: {
+        backgroundColor: '#EBEBEB',
+        border: '0',
+      },
+    },
+    MuiPaper: {
+      root: {
+        backgroundColor: '#fdfdfd',
+      },
+    },
+    MuiContainer: {
+      root: {
+        maxWidth: '800px',
+      },
+    },
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <ThemeProvider theme={theme}>
+          <UserProvider>
+            <App />
+          </UserProvider>
+        </ThemeProvider>
+      </LocalizationProvider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
