5326401b60e1e8fff44eaaa225f61f12d4e52732
Taylor <taylor@crucialartists.com>
2023-03-20 12:17:33 -0500

    Add validations to adventure model and update attributes in adventure serializer

--- app/models/adventure.rb
+++ app/models/adventure.rb
@@ -2,7 +2,12 @@ class Adventure < ApplicationRecord
 
     belongs_to :user
     belongs_to :cat
-    has_many :adventure_likes
+    has_many :adventure_likes, dependent: :destroy
+
+    validates :title, presence: true
+    validates :description, presence: true
+    validates :location, presence: true
+    validates :cat_id, presence: true
 
     def total_likes
         self.adventure_likes.count
--- app/serializers/adventure_serializer.rb
+++ app/serializers/adventure_serializer.rb
@@ -1,5 +1,5 @@
 class AdventureSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :cat_id, :cat, :user, :title, :description, :location, :total_likes
+  attributes :id, :user_id, :cat_id, :cat, :user, :title, :description, :location, :total_likes, :created_at
 
   belongs_to :user
   belongs_to :cat
