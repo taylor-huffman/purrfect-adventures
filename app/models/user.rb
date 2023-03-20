74ed2d9ea899db35877cf445d8cd90510a6bc26b
Taylor <taylor@crucialartists.com>
2023-03-20 12:31:13 -0500

    Add user associations and validations, update user associations in user serializer

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
     has_secure_password
 
-    has_many :adventures
-    has_many :cats, through: :adventures
-    has_many :adventure_likes, through: :adventures
+    has_many :adventures, dependent: :destroy
+    has_many :cats, -> { distinct }, through: :adventures, dependent: :destroy
+    has_many :adventure_likes, dependent: :destroy
+    # has_many :adventure_likes, through: :adventures
+
+    validates :username, presence: true, uniqueness: true
+    validates :bio, presence: true
 end
