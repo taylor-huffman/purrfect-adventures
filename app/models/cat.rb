16955fa3bba91347e46ad6e808b1f52bcb7ead1f
Taylor <taylor@crucialartists.com>
2023-03-20 12:25:09 -0500

    Add validations to cat model and update attributes in cat serializer

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,6 +1,11 @@
 class Cat < ApplicationRecord
 
-    has_many :adventures
+    has_many :adventures, dependent: :destroy
     has_many :users, through: :adventures
+
+    validates :name, presence: true
+    validates :birthdate, presence: true
+    validates :breed, presence: true
+    validates :favorite_toy, presence: true
 
 end
--- app/serializers/cat_serializer.rb
+++ app/serializers/cat_serializer.rb
@@ -1,5 +1,5 @@
 class CatSerializer < ActiveModel::Serializer
-  attributes :id, :name, :birthdate, :favorite_toy
+  attributes :id, :name, :birthdate, :favorite_toy, :breed
 
   has_many :adventures
   has_many :users, through: :adventures
