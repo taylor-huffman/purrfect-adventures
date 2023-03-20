d665a542f94fe080aa71d566ced6b99cf6a41b05
Taylor <taylor@crucialartists.com>
2023-03-20 12:24:18 -0500

    Build out cats controller methods

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -1,2 +1,36 @@
 class CatsController < ApplicationController
+
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
+    def index
+        cats = Cat.all
+        render json: cats
+    end
+
+    def create
+        cat = Cat.create!(user_params)
+        render json: cat
+    end
+
+    def destroy
+        cat = Cat.find(params[:id])
+        cat.destroy
+        head :no_content
+    end
+
+    def random
+        cat = Cat.order(Arel.sql('RANDOM()')).first
+        render json: cat
+    end
+
+    private
+
+    def user_params
+        params.permit(:name, :birthdate, :favorite_toy, :breed)
+    end
+
+    def render_unprocessable_entity_response(invalid)
+        render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+    end
+
 end
