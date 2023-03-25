15157e6a53c53832e3fb9fd6fbca32f7afcbf5c4
Taylor <taylor@crucialartists.com>
2023-03-24 20:25:42 -0500

    Refactor create action to replace if/else statement

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,12 +16,8 @@ class UsersController < ApplicationController
 
     def create
         user = User.create!(user_params)
-        if user.valid?
-            session[:user_id] = user.id
-            render json: user
-        else
-            render :render_unprocessable_entity_response
-        end
+        session[:user_id] = user.id
+        render json: user
     end
 
     private
