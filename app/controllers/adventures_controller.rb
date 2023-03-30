b550a767a1e5945ccc15392df0e133d10ca632dd
Taylor <taylor@crucialartists.com>
2023-03-29 22:02:40 -0500

    Add render not found response to adventures controller, remove validates :user_id from adventure model

--- app/controllers/adventures_controller.rb
+++ app/controllers/adventures_controller.rb
@@ -1,5 +1,6 @@
 class AdventuresController < ApplicationController
 
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
     rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
     before_action :authorize
 
@@ -9,26 +10,34 @@ class AdventuresController < ApplicationController
     end
 
     def create
-        if params[:user_id] == session[:user_id]
-            adventure = Adventure.create!(user_params)
-            render json: adventure
-        else
-            render json: { errors: ["Sorry, you're not authorized"] }, status: :unauthorized
-        end
+        # if params[:user_id] == session[:user_id]
+        #     adventure = Adventure.create!(user_params)
+        #     render json: adventure
+        # else
+        #     render json: { errors: ["Sorry, you're not authorized"] }, status: :unauthorized
+        # end
+        user = User.find(session[:user_id])
+        adventure = user.adventures.create!(user_params)
+        render json: adventure
     end
 
     def update
-        adventure = Adventure.find(params[:id])
-        if params[:user_id] == session[:user_id]
-            adventure.update!(user_params)
-            render json: adventure
-        else
-            render json: { errors: ["Sorry, you're not authorized"] }, status: :unauthorized
-        end
+        # adventure = Adventure.find(params[:id])
+        # if params[:user_id] == session[:user_id]
+        #     adventure.update!(user_params)
+        #     render json: adventure
+        # else
+        #     render json: { errors: ["Sorry, you're not authorized"] }, status: :unauthorized
+        # end
+        user = User.find(session[:user_id])
+        adventure = user.adventures.find(params[:id])
+        adventure.update!(user_params)
+        render json: adventure
     end
 
     def destroy
-        adventure = Adventure.find(params[:id])
+        user = User.find(session[:user_id])
+        adventure = user.adventures.find(params[:id])
         adventure.destroy
         head :no_content
     end
@@ -45,6 +54,10 @@ class AdventuresController < ApplicationController
 
     def render_unprocessable_entity_response(invalid)
         render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+    end
+
+    def render_not_found_response(error)
+        render json: { errors: [error.message] }, status: :not_found
     end
 
 end
