634c02a91f342956ec7440c37604da4165de6232
Taylor <taylor@crucialartists.com>
2023-04-04 12:33:20 -0500

    Add new custom route /mostlikes for practice

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   delete "/logout", to: "sessions#destroy"
   get "/auth", to: "users#show"
   get "/randomcat", to: "cats#random"
+  get "/mostlikes", to: "adventures#most_likes"
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
